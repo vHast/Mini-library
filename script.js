ea8999a8bb949fbb65082991b0c2c26424abe72b
vHast <fedvhast@gmail.com>
2023-03-04 20:17:46 +0200

    Added addBook functionality

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Need to store all book names inside the myLibrary array
-
-let myLibrary = []
+// We initialize the listeners for the buttons and forms
 
 mainButtonContent = document.querySelector('#first-btn');
 mainFormContent = document.querySelector("#main-form")
+
+// This will modify the CSS in order to display certain elements of the DOM, will hide the main button and then show the main form
 
 showButton = () => {
   mainButtonContent.style.display = "none"
@@ -13,12 +13,30 @@ showButton = () => {
 const testNames = () => {
   console.log(document.getElementById('bookName'));
 }
+
+// Need to store all book names inside the myLibrary array
+
+let myLibrary = []
+
+
 // Function to add the books, should be added to a button or 
 
-const addBook = () => {
-  let title = document.getElementById('bookName');
-  let author = document.getElementById('bookAuthor');
-  let year = document.getElementById('bookYear');
+const addBook = (form) => {
+  
+  let name = form.fname.value;
+  let author = form.lname.value;
+  let year = form.yname.value;
+  
+  let newBook = {
+    nameBook : name,
+    authorBook : author,
+    yearBook : year
+  }
 
-  console.log(title, author, year);
+  myLibrary.push(newBook)
+  form.reset();
+  console.log(myLibrary);
+
+  // This function will return the value of each input fields that have been put
+
 };
