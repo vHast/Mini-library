6081afe8500d7a076dff5768a1b9b6a9334e6901
vHast <fedvhast@gmail.com>
2023-03-04 19:53:26 +0200

    Hiding main button function

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-const addBook = () => {
+// Need to store all book names inside the myLibrary array
 
+let myLibrary = []
+
+mainButtonContent = document.querySelector('#first-btn');
+mainFormContent = document.querySelector("#main-form")
+
+showButton = () => {
+  mainButtonContent.style.display = "none"
+  mainFormContent.style.display = "flex"
 }
+
+const testNames = () => {
+  console.log(document.getElementById('bookName'));
+}
+// Function to add the books, should be added to a button or 
+
+const addBook = () => {
+  let title = document.getElementById('bookName');
+  let author = document.getElementById('bookAuthor');
+  let year = document.getElementById('bookYear');
+
+  console.log(title, author, year);
+};
